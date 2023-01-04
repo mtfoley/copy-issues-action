d7f815cb7f221a9bcb570598572076e855acb7c9
Matthew <mtfoley.mae@gmail.com>
2023-01-04 04:15:54 +0000

    octokit setup

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
+const {context} = require('@actions/github/lib/utils');
 const wait = require('./wait');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
+    const token = core.getInput('token');
+    const client = github.getOctokit(token);
+    // const ctx = github.context
+    const {is_template, fork, template_repository} = context.repo;
+    
+    client.paginate(client.rest.issues.listForRepo, {
+      owner: "mtfoley",
+      repo: "pr-compliance-action",
+    })
+    .then((issues) => {
+      core.debug(`Found ${issues.length} issues`);
+    });
+
+    core.debug({is_template, fork, template_repository});
     const ms = core.getInput('milliseconds');
     core.info(`Waiting ${ms} milliseconds ...`);
 
